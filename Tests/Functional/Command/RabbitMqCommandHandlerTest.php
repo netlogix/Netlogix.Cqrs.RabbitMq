cf8ba2ce5551572304ce8a27e7152702d603638c
Lienhart Woitok <lienhart.woitok@netlogix.de>
2016-06-10 14:39:39 +0200

    [TASK] Add test for command bus integration

--- Tests/Functional/Command/RabbitMqCommandHandlerTest.php
+++ Tests/Functional/Command/RabbitMqCommandHandlerTest.php
@@ -5,6 +5,7 @@ namespace Netlogix\Cqrs\RabbitMq\Tests\Functional\Command;
  * This file is part of the Netlogix. package.
  */
 
+use Netlogix\Cqrs\Command\CommandBus;
 use Netlogix\Cqrs\RabbitMq\Command\RabbitMqCommandHandler;
 use Netlogix\Cqrs\RabbitMq\Tests\Functional\Fixtures\AsynchronousTestCommand;
 use PhpAmqpLib\Channel\AMQPChannel;
@@ -76,4 +77,23 @@ class RabbitMqCommandHandlerTest extends \TYPO3\Flow\Tests\FunctionalTestCase {
 		});
 		$this->amqpChannel->wait(NULL, FALSE, 1);
 	}
+
+	public function testRabbitMqCommandHandlerIsUsedByCommandBus() {
+		if ($this->amqpChannel === NULL) {
+			$this->markTestSkipped('RabbitMq server is not available');
+		}
+
+		$command = new AsynchronousTestCommand('3b2635c5-f261-4d02-ae5f-6e98df52bff1', 'foobar');
+
+		$commandBus = new CommandBus();
+		$commandBus->delegate($command);
+
+		$test = $this;
+		$this->amqpChannel->basic_consume($this->amqpQueue, '', FALSE, FALSE, FALSE, FALSE, function (\PhpAmqpLib\Message\AMQPMessage $msg) use ($test) {
+			$test->assertJson($msg->body);
+		});
+		$this->amqpChannel->wait(NULL, FALSE, 1);
+
+	}
+
 }
